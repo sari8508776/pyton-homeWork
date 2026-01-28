83e387cb2ac92bee205c7b506456e15964e0a297
sara <sari8508776@gmail.com>
2026-01-28 20:21:05 +0200

    little change

--- פייתון מתקדם/שיעור1/main.py
+++ פייתון מתקדם/שיעור1/main.py
@@ -16,28 +16,49 @@ def say_hi():
 print(say_hi())
 
 # ex2
+# def deco2(func):
+#     memo = {}
+#     def wrapper2(n):
+#         start = time.perf_counter()
+#         if n not in memo:
+#             memo[n] = func(n)
+#         end = time.perf_counter()
+#         print(end-start)
+#         return memo.get(n)
+#     return wrapper2
+# @deco2
+# def fibonacci(n):
+#     a, b = 0, 1
+#     count = 0
+#     while count < n:
+#         print(a, end=" ")
+#         a, b = b, a + b
+#         count += 1
+#     return  b
+# print(fibonacci(10))
+# print(fibonacci(10))
+
+
 def deco2(func):
     memo = {}
-    def wrapper2(n):
-        start = time.perf_counter()
-        if n not in memo:
-            memo[n] = func(n)
-        end = time.perf_counter()
-        print(end-start)
-        return memo.get(n)
-    return wrapper2
+    def wrapper2(*args):
+        if args not in memo:
+            memo[args] = func(*args)
+        return memo[args]
+    return wrapper
+
 @deco2
 def fibonacci(n):
     a, b = 0, 1
     count = 0
     while count < n:
-        print(a, end=" ")
         a, b = b, a + b
         count += 1
-    return  b
-print(fibonacci(10))
-print(fibonacci(10))
+    return a
 
+print(fibonacci(10))
+print(fibonacci(10))
+print(fibonacci(5)
 
 
 
